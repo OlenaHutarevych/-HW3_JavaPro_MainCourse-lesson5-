b22f95c8ba97092dd1bb4cd04a58e54bf270709b
Olena Hutarevych <olenahutarevych@gmail.com>
2023-04-19 13:35:19 +0300

    add the text of HW-lesson6 in comments

--- src/main/java/com/example/projecthw5/ProjectHw5Application.java
+++ src/main/java/com/example/projecthw5/ProjectHw5Application.java
@@ -7,6 +7,13 @@ package com.example.projecthw5;
 //        4. Добавить аннотацию @Data над описанием класса для цитаті(Quote). Єто аннотация из библиотеки lombok, которая автоматически вставляет в класс getters/setters/toString/equals/hashCode. Теперь не нужно будет писать єто вручную или автогенерировать.
 //        5. Доделать домашки из прошлого занятия.
 
+//Домашнее задание 6:
+
+//2. Попробовать собрать нашу программу через коммандную строку(в корне приложения через terminal віполнить 'mvn clean install') а также через интерфейс idea.
+//        3. Добавить библиотеку lombok к нашему проекту: https://mvnrepository.com/artifact/org.projectlombok/lombok
+//        4. Добавить аннотацию @Data над описанием класса для цитаті(Quote). Єто аннотация из библиотеки lombok, которая автоматически вставляет в класс getters/setters/toString/equals/hashCode. Теперь не нужно будет писать єто вручную или автогенерировать.
+//        5. Доделать домашки из прошлого занятия.
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
