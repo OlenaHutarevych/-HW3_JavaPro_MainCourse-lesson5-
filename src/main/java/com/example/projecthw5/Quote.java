6916f52da178c88bba0493756b629fe657b5b4cb
Olena Hutarevych <olenahutarevych@gmail.com>
2023-04-19 13:42:26 +0300

    add the dependency lombok to pom.xml, add the lombok lybrary and @Data

--- src/main/java/com/example/projecthw5/Quote.java
+++ src/main/java/com/example/projecthw5/Quote.java
@@ -3,22 +3,24 @@ package com.example.projecthw5;
 // ця структура для зберігання того, аби могли введену цитату обгортати в JSON і додавати в quotes, передавати і ті
 
 import jakarta.validation.constraints.Size;// імпорт анотації валідації по розміру
-
+import lombok.Data;
+@Data
 public class Quote
 {
     @Size(max = 40,  message = "Size should be max 40 chars") // вводимо валідацію по розміру, тут вказали 40 символів,
     // тепер Spring буде автоматом перевіряти цитату на розмір, чи менший він 40
 
     private String quote;
+    //додали анотацію @Data, тож гетери і сетери вже можна не писати
 
-    public String getQuote()
-    {
-        return quote;
-    }
-
-    public void setQuote(String quote)
-    {
-        this.quote = quote;
-    }
+//    public String getQuote()
+//    {
+//        return quote;
+//    }
+//
+//    public void setQuote(String quote)
+//    {
+//        this.quote = quote;
+//    }
 
   }
